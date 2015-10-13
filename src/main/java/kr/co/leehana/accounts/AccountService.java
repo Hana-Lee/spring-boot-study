731c0f531b00c1bd98b2170d017a1e7e3e5beaa5
Hana Lee <i@leehana.co.kr>
2015-10-13 15:23:11 +0900

    모든 public 메소드가 transactional 적용 받을 수 있도록 annotation 추가.

--- src/main/java/kr/co/leehana/accounts/AccountService.java
+++ src/main/java/kr/co/leehana/accounts/AccountService.java
@@ -2,6 +2,7 @@ package kr.co.leehana.accounts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Hana Lee on 2015-10-13 오후 3:16
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
  * @since 2015-10-13 오후 3:16
  */
 @Service
+@Transactional
 public class AccountService {
 
 	@Autowired
